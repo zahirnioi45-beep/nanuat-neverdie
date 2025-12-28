22a75ac9d6f2c06cd573127263ffd7fc9f5d8da4
Zahir Nioi <zahirnioi45@gmail.com>
2025-12-28 19:24:27 +0700

    Fix all filenames to match HTML

---
+++ fix_filenames.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Folder girls
+cd img/girls
+mv papaw.jpg Papaw.jpg
+mv biu.jpg Biu.jpg
+mv atu.jpg Atu.jpg
+mv edera.jpg Edera.jpg
+mv flo.jpg Flo.jpg
+mv cinde.jpg Cinde.jpg
+mv cimit.jpg Cimit.jpg
+mv "miaw epep.jpg" "Miaw Epep.jpg"
+mv yasmin.jpg Yasmin.jpg
+mv jie.jpg Jie.jpg
+mv aza.jpg Aza.jpg
+mv ice.jpg Ice.jpg
+mv meongcit.jpg Meongcit.jpg
+
+# Folder boys
+cd ../boys
+mv raxi.jpg Raxi.jpg
+mv mir.jpg Mir.jpg
+mv arlot.jpg Arlot.jpg
+mv jopa.jpg Jopa.jpg
+mv ciku.jpg Ciku.jpg
+mv darl.jpg Darl.jpg
+mv rojak.jpg Rojak.jpg
+mv antaraksa.jpg Antaraksa.jpg
+mv ilo.jpg Ilo.jpg
+mv "taka mdn.jpg" "Taka Mdn.jpg"
+mv getsu.jpg Getsu.jpg
